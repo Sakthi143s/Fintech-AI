af5b029654403bdce569537a7c16938d4b0d8434
Sakthiswaran <sakthiswaran451@gmail.com>
2026-03-08 23:57:50 +0530

    fix(backend): allow all origins for CORS to fix vercel requests

--- backend/main.py
+++ backend/main.py
@@ -16,13 +16,7 @@ app = FastAPI(
 # CORS — allow frontend dev server
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:5173", 
-        "http://127.0.0.1:5173",
-        "http://localhost:5174",
-        "http://127.0.0.1:5174",
-        "http://localhost:5175"
-    ],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
